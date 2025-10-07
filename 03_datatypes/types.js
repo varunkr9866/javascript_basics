9f386a90ee4b74f5f32f89aecddc7fecbd29f70a
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-07 13:03:36 +0530

    COMPLETED VAR,CONST,LET IN  JS

---
+++ 03_datatypes/types.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = "0";
+let c = 10.43;
+let d = null;
+let e = undefined;
+{
+    let b = 'this';
+    console.log(b);
+}
+console.log(b);
+
+const author = "Varun";
+console.log(author);
