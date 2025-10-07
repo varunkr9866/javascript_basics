abffcfd6e6a4724fbd103839d70b5938e3a74f39
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-10-07 14:28:29 +0530

    COMPLETED OBJECTS AND PRIMITIVE DATA TYPE SIN JS

---
+++ primitives.js
@@ -0,0 +1,21 @@
+//nn bb ss u
+let a = null;
+let b = 100;
+let c = true;
+let d = BigInt("1000");
+let e = 'String';
+let f = Symbol('I AM VARUN');
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
+
+console.log(typeof(e));
+
+//Objects in JS
+const items = {
+    "Varun" : true,
+    "Rohan" : false,
+    "age" : 18,
+    "Bemma" : undefined,
+}
+console.log(items["Varun"]);
